563f1ad1ece06430ea9604a59befa64119ff1297
Paul Kim <paul.eun.kim@gmail.com>
2020-10-04 22:49:51 -0600

    added pic

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import HeartIcon from '../assets/heart.svg';
 import SocialIcon from './social_icon';
+
 
 const Footer = ({ social }) => (
   <footer className="py-6 mx-auto items-center justify-between md:flex">
